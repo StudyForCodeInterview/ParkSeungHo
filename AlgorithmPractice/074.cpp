20409aa5aa8e0b866239a0344cdf3ae0bf347c4e
seungpar <seungpar@gmail.com>
2023-11-23 21:41:59 +0900

    231123

--- AlgorithmPractice/074.cpp
+++ AlgorithmPractice/074.cpp
@@ -1,8 +1,36 @@
-// 최소 공통 조상
+// 최소 공통 조상 (11437)
 #define _CRT_SECURE_NO_WARNINGS
-//#include <iostream>
-//
-//int main(void)
-//{
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> tree;
+
+int LCA(int n1, int n2);
+
+int main(void)
+{
+    int i;
+    int N, M;
+    int n1, n2;
+    scanf("%d", &N);
+    vec.resize(N + 1);
+
+    for (i = 0; i < N - 1; i++)
+    {
+        scanf("%d %d", &n1, &n2);
+    }
+
+    scanf("%d", &M);
+    for (i = 0; i < M; i++)
+    {
+        scanf("%d %d", &n1, &n2);
+        printf("%d\n", LCA(n1, n2));
+    }
+    return 0;
+}
+
+int LCA(int n1, int n2)
+{
+
+}
