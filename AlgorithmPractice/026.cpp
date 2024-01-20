b42483d85a29bf300d1768749c46020b72e45fb6
seungpar <seungpar@gmail.com>
2024-01-20 17:11:28 +0900

    240120

--- AlgorithmPractice/026.cpp
+++ AlgorithmPractice/026.cpp
@@ -13,12 +13,6 @@ void DFS_Stack(int V)
 {
     visited[V] = true;
     printf("%d ", V);   
-    /*vector<int>::iterator it;
-    for (it = adjustList[V].begin(); it != adjustList[V].end(); it++)
-    {
-        if (visited[*it] == false)
-            DFS(*it);
-    }*/
     for (int i : adjustList[V])
     {
         if (visited[i] == false)
@@ -92,4 +86,79 @@ int main(void)
     fill(visited.begin(), visited.end(), 0);
     BFS(V);
     return 0;
+}
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+vector<bool> visited;
+vector<vector<int>> A;
+stack<int> st;
+
+void DFS(int node);
+void BFS(int node);
+int main(void)
+{
+    int N, M, V;
+    int n1, n2;
+
+    cin >> N >> M >> V;
+    visited.resize(N + 1, false);
+    A.resize(N + 1);
+    for (int i = 1; i <= M; i++)
+    {
+        cin >> n1 >> n2;
+        A[n1].push_back(n2);
+        A[n2].push_back(n1);
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        sort(A[i].begin(), A[i].end());
+    }
+
+    DFS(V);
+    cout << endl;
+    fill(visited.begin(), visited.end(), false);
+    BFS(V);
+    return 0;
+}
+
+void DFS(int node)
+{
+    if (visited[node] == true)
+        return;
+
+    visited[node] = true;
+    cout << node << " ";
+    for (int i : A[node])
+    {
+        DFS(i);
+    }
+}
+
+void BFS(int node)
+{
+    queue<int> q;
+
+    visited[node] = true;
+    q.push(node);
+    while (!q.empty())
+    {
+        int tmp = q.front();
+        q.pop();
+        cout << tmp << " ";
+        for (int i : A[tmp])
+        {
+            if (visited[i] != true)
+            {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+    cout << endl;
 }
